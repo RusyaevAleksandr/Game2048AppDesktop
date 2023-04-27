d9d87f0493194385033b27139dd61b524230478e
RusyaevAleksandr <rusyaev1985@yandex.ru>
2023-04-27 14:00:04 +0500

    Добавил класс для работы с файловой системой

--- Common/FileProvider.cs
+++ Common/FileProvider.cs
@@ -1,6 +1,65 @@
-﻿namespace Common
+﻿using System.Text;
+
+namespace Common
 {
-    internal class FileProvider
+    public class FileProvider
     {
+        private const string path = @"Settings";
+
+        private const string nameFileResultGame = @"Settings\ResultGame.json";
+
+        public string Path
+        {
+            get { return path; }
+        }
+        public string NameFileResultGame
+        {
+            get { return nameFileResultGame; }
+        }
+
+        public void AppendToFile(string path, string nameFile, string value)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            if (!directoryInfo.Exists)
+            {
+                directoryInfo.Create();
+            }
+            using(StreamWriter sw = new StreamWriter(nameFile, true, Encoding.Default))
+            {
+                sw.WriteLine(value);
+            }
+        }
+        public static void Replace(string paht, string nameFile, string value)
+        {
+            DirectoryInfo directory = new DirectoryInfo(paht);
+
+            if (!directory.Exists)
+            {
+                directory.Create();
+            }
+
+            using (StreamWriter sw = new StreamWriter(nameFile, false, Encoding.Default))
+            {
+                sw.WriteLine(value);
+            }
+        }
+        public static string GetValue(string nameFile)
+        {
+            using (StreamReader sr = new StreamReader(nameFile, Encoding.Default))
+            {
+                string textResult = sr.ReadToEnd();
+                return textResult;
+            }
+        }
+
+        public static bool Exists(string nameFile)
+        {
+            return File.Exists(nameFile);
+        }
+        internal static void Clear(string nameFile)
+        {
+            File.WriteAllText(nameFile, string.Empty);
+        }
     }
 }
