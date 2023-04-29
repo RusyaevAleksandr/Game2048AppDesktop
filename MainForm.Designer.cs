0221dfbb295073fa36f455e0ca3cc3df4550884c
RusyaevAleksandr <rusyaev1985@yandex.ru>
2023-04-29 12:20:04 +0500

    Изменил внешние элементы на формах. Добавил доп надписи на главной форме.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -45,10 +45,13 @@
             tableLayoutPanelTop = new TableLayoutPanel();
             tableLayoutPanel2 = new TableLayoutPanel();
             tableLayoutPanel3 = new TableLayoutPanel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            infoTextLabel = new Label();
             menuStrip.SuspendLayout();
             tableLayoutPanelTop.SuspendLayout();
             tableLayoutPanel2.SuspendLayout();
             tableLayoutPanel3.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // scoreTextLabel
@@ -59,7 +62,7 @@
             scoreTextLabel.Location = new Point(3, 0);
             scoreTextLabel.Name = "scoreTextLabel";
             scoreTextLabel.Padding = new Padding(4, 0, 0, 0);
-            scoreTextLabel.Size = new Size(54, 19);
+            scoreTextLabel.Size = new Size(54, 24);
             scoreTextLabel.TabIndex = 0;
             scoreTextLabel.Text = "Счет:";
             // 
@@ -70,67 +73,70 @@
             scoreLabel.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             scoreLabel.Location = new Point(89, 0);
             scoreLabel.Name = "scoreLabel";
-            scoreLabel.Size = new Size(19, 19);
+            scoreLabel.Size = new Size(19, 24);
             scoreLabel.TabIndex = 1;
             scoreLabel.Text = "0";
             // 
             // menuStrip
             // 
             menuStrip.BackColor = Color.PaleGoldenrod;
+            menuStrip.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             menuStrip.Items.AddRange(new ToolStripItem[] { menuToolStripMenuItem, helpToolStripMenuItem });
             menuStrip.Location = new Point(0, 0);
             menuStrip.Name = "menuStrip";
             menuStrip.Padding = new Padding(5, 2, 0, 2);
-            menuStrip.Size = new Size(358, 24);
+            menuStrip.Size = new Size(358, 29);
             menuStrip.TabIndex = 2;
             menuStrip.Text = "menuStrip1";
             // 
             // menuToolStripMenuItem
             // 
             menuToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { restartGameToolStripMenuItem, saveResultToolStripMenuItem, resultListToolStripMenuItem, exitToolStripMenuItem });
+            menuToolStripMenuItem.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             menuToolStripMenuItem.Name = "menuToolStripMenuItem";
-            menuToolStripMenuItem.Size = new Size(53, 20);
+            menuToolStripMenuItem.Size = new Size(66, 25);
             menuToolStripMenuItem.Text = "Меню";
             // 
             // restartGameToolStripMenuItem
             // 
             restartGameToolStripMenuItem.Name = "restartGameToolStripMenuItem";
-            restartGameToolStripMenuItem.Size = new Size(189, 22);
+            restartGameToolStripMenuItem.Size = new Size(230, 26);
             restartGameToolStripMenuItem.Text = "Начать сначала";
             restartGameToolStripMenuItem.Click += restartGameToolStripMenuItem_Click;
             // 
             // saveResultToolStripMenuItem
             // 
             saveResultToolStripMenuItem.Name = "saveResultToolStripMenuItem";
-            saveResultToolStripMenuItem.Size = new Size(189, 22);
+            saveResultToolStripMenuItem.Size = new Size(230, 26);
             saveResultToolStripMenuItem.Text = "Сохранить результат";
             saveResultToolStripMenuItem.Click += saveResultToolStripMenuItem_Click;
             // 
             // resultListToolStripMenuItem
             // 
             resultListToolStripMenuItem.Name = "resultListToolStripMenuItem";
-            resultListToolStripMenuItem.Size = new Size(189, 22);
+            resultListToolStripMenuItem.Size = new Size(230, 26);
             resultListToolStripMenuItem.Text = "Список результатов";
             resultListToolStripMenuItem.Click += resultListToolStripMenuItem_Click;
             // 
             // exitToolStripMenuItem
             // 
             exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            exitToolStripMenuItem.Size = new Size(189, 22);
+            exitToolStripMenuItem.Size = new Size(230, 26);
             exitToolStripMenuItem.Text = "Выход";
             exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
             // 
             // helpToolStripMenuItem
             // 
             helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { rulesGameToolStripMenuItem });
+            helpToolStripMenuItem.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            helpToolStripMenuItem.Size = new Size(68, 20);
+            helpToolStripMenuItem.Size = new Size(83, 25);
             helpToolStripMenuItem.Text = "Помощь";
             // 
             // rulesGameToolStripMenuItem
             // 
             rulesGameToolStripMenuItem.Name = "rulesGameToolStripMenuItem";
-            rulesGameToolStripMenuItem.Size = new Size(153, 22);
+            rulesGameToolStripMenuItem.Size = new Size(180, 26);
             rulesGameToolStripMenuItem.Text = "Правила игры";
             rulesGameToolStripMenuItem.Click += rulesGameToolStripMenuItem_Click;
             // 
@@ -142,7 +148,7 @@
             userLabel.Location = new Point(3, 0);
             userLabel.Name = "userLabel";
             userLabel.Padding = new Padding(4, 2, 0, 0);
-            userLabel.Size = new Size(68, 25);
+            userLabel.Size = new Size(68, 30);
             userLabel.TabIndex = 3;
             userLabel.Text = "Игрок:";
             // 
@@ -154,7 +160,7 @@
             bestScoreTextLabel.Location = new Point(238, 0);
             bestScoreTextLabel.Name = "bestScoreTextLabel";
             bestScoreTextLabel.Padding = new Padding(0, 2, 4, 0);
-            bestScoreTextLabel.Size = new Size(117, 25);
+            bestScoreTextLabel.Size = new Size(117, 30);
             bestScoreTextLabel.TabIndex = 4;
             bestScoreTextLabel.Text = "Лучший счет";
             // 
@@ -165,7 +171,7 @@
             scoreTextLabel2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             scoreTextLabel2.Location = new Point(33, 0);
             scoreTextLabel2.Name = "scoreTextLabel2";
-            scoreTextLabel2.Size = new Size(50, 19);
+            scoreTextLabel2.Size = new Size(50, 24);
             scoreTextLabel2.TabIndex = 5;
             scoreTextLabel2.Text = "Счет:";
             // 
@@ -177,7 +183,7 @@
             bestScoreLabel.Location = new Point(147, 0);
             bestScoreLabel.Name = "bestScoreLabel";
             bestScoreLabel.Padding = new Padding(0, 0, 4, 0);
-            bestScoreLabel.Size = new Size(23, 19);
+            bestScoreLabel.Size = new Size(23, 24);
             bestScoreLabel.TabIndex = 6;
             bestScoreLabel.Text = "0";
             // 
@@ -191,13 +197,13 @@
             tableLayoutPanelTop.Controls.Add(bestScoreTextLabel, 1, 0);
             tableLayoutPanelTop.Controls.Add(tableLayoutPanel3, 1, 1);
             tableLayoutPanelTop.Dock = DockStyle.Top;
-            tableLayoutPanelTop.Location = new Point(0, 24);
+            tableLayoutPanelTop.Location = new Point(0, 29);
             tableLayoutPanelTop.MinimumSize = new Size(241, 50);
             tableLayoutPanelTop.Name = "tableLayoutPanelTop";
             tableLayoutPanelTop.RowCount = 2;
-            tableLayoutPanelTop.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanelTop.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanelTop.Size = new Size(358, 50);
+            tableLayoutPanelTop.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanelTop.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanelTop.Size = new Size(358, 60);
             tableLayoutPanelTop.TabIndex = 7;
             // 
             // tableLayoutPanel2
@@ -208,11 +214,11 @@
             tableLayoutPanel2.Controls.Add(scoreTextLabel, 0, 0);
             tableLayoutPanel2.Controls.Add(scoreLabel, 1, 0);
             tableLayoutPanel2.Dock = DockStyle.Fill;
-            tableLayoutPanel2.Location = new Point(3, 28);
+            tableLayoutPanel2.Location = new Point(3, 33);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 1;
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel2.Size = new Size(173, 19);
+            tableLayoutPanel2.Size = new Size(173, 24);
             tableLayoutPanel2.TabIndex = 4;
             // 
             // tableLayoutPanel3
@@ -223,12 +229,38 @@
             tableLayoutPanel3.Controls.Add(scoreTextLabel2, 0, 0);
             tableLayoutPanel3.Controls.Add(bestScoreLabel, 1, 0);
             tableLayoutPanel3.Dock = DockStyle.Fill;
-            tableLayoutPanel3.Location = new Point(182, 28);
+            tableLayoutPanel3.Location = new Point(182, 33);
             tableLayoutPanel3.Name = "tableLayoutPanel3";
             tableLayoutPanel3.RowCount = 1;
             tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel3.Size = new Size(173, 19);
+            tableLayoutPanel3.Size = new Size(173, 24);
             tableLayoutPanel3.TabIndex = 5;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 3.46593332F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 93.06813F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 3.465934F));
+            tableLayoutPanel1.Controls.Add(infoTextLabel, 1, 0);
+            tableLayoutPanel1.Dock = DockStyle.Top;
+            tableLayoutPanel1.Location = new Point(0, 89);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(358, 31);
+            tableLayoutPanel1.TabIndex = 8;
+            // 
+            // infoTextLabel
+            // 
+            infoTextLabel.AutoSize = true;
+            infoTextLabel.Dock = DockStyle.Fill;
+            infoTextLabel.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            infoTextLabel.Location = new Point(15, 0);
+            infoTextLabel.Name = "infoTextLabel";
+            infoTextLabel.Size = new Size(327, 31);
+            infoTextLabel.TabIndex = 0;
+            infoTextLabel.Text = "Соберите число 2048 на одной плтитке!";
             // 
             // MainForm
             // 
@@ -236,6 +268,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Info;
             ClientSize = new Size(358, 450);
+            Controls.Add(tableLayoutPanel1);
             Controls.Add(tableLayoutPanelTop);
             Controls.Add(menuStrip);
             Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
@@ -253,6 +286,8 @@
             tableLayoutPanel2.PerformLayout();
             tableLayoutPanel3.ResumeLayout(false);
             tableLayoutPanel3.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -276,5 +311,7 @@
         private TableLayoutPanel tableLayoutPanelTop;
         private TableLayoutPanel tableLayoutPanel2;
         private TableLayoutPanel tableLayoutPanel3;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label infoTextLabel;
     }
 }
